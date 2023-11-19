71778568461e6cd4810ff50e7185ed5d3d84c0a6
Andres <andrumen1@yahoo.com>
2023-11-18 23:53:50 -0600

    Changes to files to work better.

--- latex_maker/src/tools/compile.py
+++ latex_maker/src/tools/compile.py
@@ -9,12 +9,10 @@
 
 
 # General
-import copy as cp
 import subprocess
 import warnings
 
 from pathlib import Path
-from typing import Union
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -28,6 +26,7 @@ from typing import Union
 
 
 _PATHLATEX = "/usr/local/texlive/2023/bin/x86_64-linux"
+
 
 # ##############################################################################
 # Functions
@@ -49,12 +48,104 @@ def _compile(path: str, configuration: dict) -> None:
 
         :param configuration: The dictionary with the configuration.
     """
-    return None
+    # Global variables.
+    global _PATHLATEX
+
+    # Extract the command.
+    mcommand = configuration["command"]
+    pcommand = f"{Path(_PATHLATEX, configuration['command'])}"
+
+    flags = configuration["flags"]
+    sescapte = configuration["shell-escape"]
+
+    # Commands to try.
+    for command in (mcommand, pcommand):
+        # Get the command.
+        tcommand = _get_command(command, flags, path, sescapte, False)
+        if _compile_run(tcommand):
+            print(
+                f"    Compilation successful! Find the output in the "
+                f"directory: {Path(path).parent}"
+            )
+            return
+
+    # If the code did not compile, try with the 'which' command.
+    tcommand = _get_command(mcommand, flags, path, sescapte, True)
+    if _compile_run(tcommand):
+        print(
+            f"    Compilation successful! Find the output in the "
+            f"directory: {Path(path).parent}"
+        )
+    return
+
+
+def _compile_run(command: list) -> bool:
+    """
+        Compiles the code using the given command and returns a boolean flag
+        indicating whether the code compiled successfully.
+
+        :param command: The list with the command to use.
+
+        :return: A boolean flag indicating whether the code compiled
+         successfully.
+    """
+    # Run the command.
+    try:
+        print(f"Compiling using the command: {' '.join(command)}")
+        for i in range(3):
+            print(f"    Run ({i + 1}/3) ...")
+            subprocess.run(command, capture_output=True, shell=False)
+
+    except FileNotFoundError:
+        print(
+            f"    The command \"{command[0]}\" was not found and the file "
+            f"\"{command[-1]}\" could not be compiled using the command: "
+            f"{' '.join(command)}."
+        )
+        return False
+
+    return True
 
 
 # ------------------------------------------------------------------------------
 # '_get' Functions
 # ------------------------------------------------------------------------------
+
+
+def _get_command(
+    command: str, flags: list, path: str, shell: bool, which: bool
+) -> list:
+    """
+        Gets the list with the given command to compile the code.
+
+        :param command: The string with the command to use.
+
+        :param flags: The list with the flags to use.
+
+        :param path: The string with the path to the file to compile.
+
+        :param shell: Whether to use the -shell-escape command to use.
+
+        :param which: A boolean flag that indicates whether to use the 'which'
+         command to get the path of the compilation command.
+
+        :return: A list with the command to compile the code.
+    """
+    # Get the path to the command.
+    tcommand = command
+    if which:
+        # Get the which command.
+        tcommand = _get_which(command)
+        tcommand = command if tcommand == "" else tcommand
+
+    # Format the flags.
+    tflags = flags
+    if shell:
+        while "-shell-escape" in tflags:
+            tflags.remove("-shell-escape")
+        tflags = ["-shell-escape"] + tflags
+
+    return [tcommand, *tflags, path]
 
 
 def _get_continue(configuration: dict, save: bool) -> bool:
@@ -84,7 +175,7 @@ def _get_continue(configuration: dict, save: bool) -> bool:
     return configuration["compile"]
 
 
-def _get_current_files(path: str, configuration: dict) -> Union[None, set]:
+def _get_current_files(path: str, configuration: dict) -> set:
     """
         Gets the list of the existing files in the given path. Will be used to
         remove them after compiling the code.
@@ -93,15 +184,15 @@ def _get_current_files(path: str, configuration: dict) -> Union[None, set]:
 
         :param configuration: The dictionary with the configuration.
 
-        :return: The list of exiting files in the parent directory of the given
-         path. None, if the files should not be removed.
+        :return: The set of exiting files in the parent directory of the given
+         path. An empty set, if the files should not be removed.
     """
     # Check if the files should be removed.
     if not configuration["remove_files"]:
-        return None
+        return set()
 
     # Get the list of files.
-    return set(f"{x}" for x in Path(path).parent.glob("*") if x.is_file())
+    return set(f"{x}" for x in Path(path).parent.glob("*"))
 
 
 def _get_which(command: str) -> str:
@@ -118,13 +209,48 @@ def _get_which(command: str) -> str:
     result = subprocess.run(command_list, capture_output=True, shell=False)
 
     if (cpath := result.stdout.decode("utf-8").strip()) == "":
-        warnings.warn(
-            f"The TeX compilation command \"{command}\" was not found using "
-            f"the \"which\" command. Alternative commands will be used."
-            , RuntimeWarning
+        print(
+            f"WARNING: The TeX compilation command \"{command}\" was not found "
+            f"using the \"which\" command. This was the last attempt to find "
+            f"the path of the command \"{command}\". Please provide the "
+            f"correct path to the compiler for the \"{command}\" compiler."
         )
 
     return cpath
+
+
+# ------------------------------------------------------------------------------
+# '_remove' Functions
+# ------------------------------------------------------------------------------
+
+
+def _remove_files(olist: set, flist: set) -> None:
+    """
+        Removes the files that were created during the compilation process.
+
+        :param olist: The list of files before compiling the code.
+
+        :param flist: The list of files after compiling the code.
+    """
+    # Exceptions.
+    fexceptions = ".tex", ".pdf", ".dvi", ".ps"
+
+    # Check if the files should be removed.
+    remv = flist - olist
+    remv = set(
+        Path(x) for x in remv if not any(x.endswith(y) for y in fexceptions)
+    )
+
+    # Nothing to remove.
+    if len(remv) == 0:
+        print("No files to remove.")
+        return
+
+    # Remove the files.
+    print("Removing files:")
+    for x in remv:
+        print(f"    Removing file: {x}")
+        x.unlink()
 
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -160,16 +286,12 @@ def compile_file(path: str, configuration: dict, save: bool) -> None:
         return
 
     # Store the list of current files.
-    flist = _get_current_files(path, configuration)
+    olist = _get_current_files(path, configuration)
 
     # Compile the file.
     _compile(path, configuration)
 
+    flist = _get_current_files(path, configuration)
+    _remove_files(olist, flist)
 
-
-
-
-
-
-
-
+    print("Done!")
