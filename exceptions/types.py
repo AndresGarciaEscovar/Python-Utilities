513cb933e45471b5cb8c69281860553eb115c510
Andres Garcia Escovar <andrumen1@yahoo.com>
2024-12-15 23:23:41 -0600

    Removed unused code.

--- exceptions/types.py
+++ exceptions/types.py
@@ -16,34 +16,6 @@ import general.strings as ustrings
 
 
 # #############################################################################
-# Functions
-# #############################################################################
-
-
-def concatenate(message: str, base: str) -> str:
-    """
-        Concatenates the message to the base message.
-
-        :param message: The message to be appended.
-
-        :param base: The base message to which the message will be appended.
-
-        :return: The concatenated message.
-    """
-    # Edge cases.
-    if message is None or message == "":
-        return base
-
-    if base.endswith("."):
-        return f"{base} {message}"
-
-    if base.strip().endswith("."):
-        return f"{base}{message}"
-
-    return f"{base}. {message}"
-
-
-# #############################################################################
 # Classes - Exceptions
 # #############################################################################
 
