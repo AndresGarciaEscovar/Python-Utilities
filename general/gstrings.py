03712bd715febb7dd7e2c334a4e4b3d7158eb3d4
Andres Garcia Escovar <andrumen1@yahoo.com>
2024-12-16 13:06:25 -0600

    Added parameter validation for the function.

--- general/gstrings.py
+++ general/gstrings.py
@@ -4,6 +4,31 @@
 
 
 # #############################################################################
+# Parameter Validation
+# #############################################################################
+
+
+def validate_messages_concat(base: str, message: str) -> None:
+    """
+        Validates the parameters for the messages_concat function are of the
+        correct type; i.e., strings.
+
+        :param base: The base message to which the message will be appended.
+
+        :param message: The message to be appended.
+
+        :raises AssertionError: If the parameters are not strings.
+    """
+    # Messages.
+    mbase: str = "The base message is not a string."
+    mmessage: str = "The message is not a string."
+
+    # Check the parameters are strings.
+    assert isinstance(base, str), mbase
+    assert isinstance(message, str), mmessage
+
+
+# #############################################################################
 # Functions
 # #############################################################################
 
@@ -18,9 +43,15 @@ def messages_concat(base: str, message: str) -> str:
 
         :return: The concatenated message.
     """
+    # Validate the parameters.
+    validate_messages_concat(base, message)
+
     # Edge cases.
     if message is None or message == "":
         return base
+
+    if base.strip() == "":
+        return message
 
     if base.endswith("."):
         return f"{base} {message}"
