0120fb96a1aa2327da752ec5e6f7a4d8a7eb6cc7
Andres Garcia Escovar <andrumen1@yahoo.com>
2024-12-18 20:03:11 -0600

    Finished the tests for the comparison functions.

--- src/utilities/.tests/validation/test_vnumbers.py
+++ src/utilities/.tests/validation/test_vnumbers.py
@@ -16,7 +16,7 @@ from typing import  Callable
 # User.
 import utilities.validation.vnumbers as vnumbers
 
-from utilities.exceptions.enumbers import NotInRangeError
+from utilities.exceptions.enumbers import AboveBelowBoundError, NotInRangeError
 
 
 # #############################################################################
@@ -32,6 +32,34 @@ class TestValidateGreaterThan(unittest.TestCase):
     # /////////////////////////////////////////////////////////////////////
     # Test Methods
     # /////////////////////////////////////////////////////////////////////
+
+    def test_bound_not_real(self):
+        """
+            Tests there is an exception if the value of the "bound"
+            parameter is not a real number.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"bound\" is a real number; it must "
+            "NOT be a real number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 1,
+            "bound": "0",
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_greater_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["bound"] = 0
+
+        vnumbers.validate_greater_than(**kwargs)
 
     def test_excpt_not_bool(self):
         """
@@ -58,6 +86,165 @@ class TestValidateGreaterThan(unittest.TestCase):
 
         # Must be a boolean.
         kwargs["excpt"] = True
+
+        vnumbers.validate_greater_than(**kwargs)
+
+    def test_include_not_bool(self):
+        """
+            Tests there is an exception if the value of the "include"
+            parameter is not a boolean.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"include\" is a boolean value; it must "
+            "NOT be a boolean number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 1,
+            "bound": 0,
+            "include": 4,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_greater_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["include"] = True
+
+        vnumbers.validate_greater_than(**kwargs)
+
+    def test_validate_greater_than(self):
+        """
+            Tests the validate_greater_than function in the module.
+        """
+        # ------------------- Value is greater than bound ------------------- #
+
+        # Messages.
+        emessage: str = (
+            "The value must be greater than the bound, and it should yield a "
+            "True result; one of these conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 1,
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must be True.
+        self.assertTrue(vnumbers.validate_greater_than(**kwargs), msg=emessage)
+
+        # ------------------ Value is greater than or equal ----------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be equal to the bound, the \"include\" flag must "
+            "be set to True, and it should yield a True result; one of these "
+            "conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": True,
+            "excpt": True,
+        }
+
+        # Must be True.
+        self.assertTrue(vnumbers.validate_greater_than(**kwargs), msg=emessage)
+
+        # ------------------------ Value is less than ----------------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be less than the bound and it should yield a "
+            "False result; one of these conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": -2,
+            "bound": 0,
+            "include": False,
+            "excpt": False,
+        }
+
+        # Must be True.
+        self.assertFalse(
+            vnumbers.validate_greater_than(**kwargs), msg=emessage
+        )
+
+        # ------------------- Value is less than or equal ------------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be equal to the bound, the \"include\" flag must "
+            "be turned off, and it should yield a False result; one of these "
+            "conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": False,
+            "excpt": False,
+        }
+
+        # Must be True.
+        self.assertFalse(
+            vnumbers.validate_greater_than(**kwargs), msg=emessage
+        )
+
+        # --------------------- Must throw an exception --------------------- #
+
+        # Messages.
+        emessage = "An exception must be raised."
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must be True.
+        with self.assertRaises(AboveBelowBoundError, msg=emessage) as _:
+            vnumbers.validate_greater_than(**kwargs)
+
+    def test_value_not_real(self):
+        """
+            Tests there is an exception if the value of the "value"
+            parameter is not a real number.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"value\" is a real number; it must "
+            "NOT be a real number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": "1",
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_greater_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["value"] = 1
 
         vnumbers.validate_greater_than(**kwargs)
 
@@ -320,6 +507,231 @@ class TestValidateInRange(unittest.TestCase):
             # Must raise an assertion error.
             with self.assertRaises(NotInRangeError, msg=emessage) as _:
                 vnumbers.validate_in_range(**kwargs)
+
+
+class TestValidateLessThan(unittest.TestCase):
+    """
+        Tests for the greater than numerical validation functions in the
+        module.
+    """
+    # /////////////////////////////////////////////////////////////////////
+    # Test Methods
+    # /////////////////////////////////////////////////////////////////////
+
+    def test_bound_not_real(self):
+        """
+            Tests there is an exception if the value of the "bound"
+            parameter is not a real number.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"bound\" is a real number; it must "
+            "NOT be a real number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": -1,
+            "bound": "0",
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_less_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["bound"] = 0
+
+        vnumbers.validate_less_than(**kwargs)
+
+    def test_excpt_not_bool(self):
+        """
+            Tests there is an exception if the value of the "excpt"
+            parameter is not a boolean.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"excpt\" is a boolean value; it must "
+            "NOT be a boolean number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": -1,
+            "bound": 0,
+            "include": False,
+            "excpt": 1,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_less_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["excpt"] = True
+
+        vnumbers.validate_less_than(**kwargs)
+
+    def test_include_not_bool(self):
+        """
+            Tests there is an exception if the value of the "include"
+            parameter is not a boolean.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"include\" is a boolean value; it must "
+            "NOT be a boolean number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": -1,
+            "bound": 0,
+            "include": 4,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_less_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["include"] = True
+
+        vnumbers.validate_less_than(**kwargs)
+
+    def test_validate_less_than(self):
+        """
+            Tests the validate_less_than function in the module.
+        """
+        # --------------------- Value is less than bound -------------------- #
+
+        # Messages.
+        emessage: str = (
+            "The value must be less than the bound, and it should yield a "
+            "True result; one of these conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": -1,
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must be True.
+        self.assertTrue(vnumbers.validate_less_than(**kwargs), msg=emessage)
+
+        # ------------------- Value is less than or equal ------------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be equal to the bound, the \"include\" flag must "
+            "be set to True, and it should yield a True result; one of these "
+            "conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": True,
+            "excpt": True,
+        }
+
+        # Must be True.
+        self.assertTrue(vnumbers.validate_less_than(**kwargs), msg=emessage)
+
+        # ---------------------- Value is greater than ---------------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be greater than the bound and it should yield a "
+            "False result; one of these conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 1,
+            "bound": 0,
+            "include": False,
+            "excpt": False,
+        }
+
+        # Must be True.
+        self.assertFalse(
+            vnumbers.validate_less_than(**kwargs), msg=emessage
+        )
+
+        # ------------------- Value is less than or equal ------------------- #
+
+        # Messages.
+        emessage = (
+            "The value must be equal to the bound, the \"include\" flag must "
+            "be turned off, and it should yield a False result; one of these "
+            "conditions is not met."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": False,
+            "excpt": False,
+        }
+
+        # Must be True.
+        self.assertFalse(
+            vnumbers.validate_less_than(**kwargs), msg=emessage
+        )
+
+        # --------------------- Must throw an exception --------------------- #
+
+        # Messages.
+        emessage = "An exception must be raised."
+
+        # Values.
+        kwargs: dict = {
+            "value": 0,
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must be True.
+        with self.assertRaises(AboveBelowBoundError, msg=emessage) as _:
+            vnumbers.validate_less_than(**kwargs)
+
+    def test_value_not_real(self):
+        """
+            Tests there is an exception if the value of the "value"
+            parameter is not a real number.
+        """
+        # Messages.
+        emessage: str = (
+            "The expected type of \"value\" is a real number; it must "
+            "NOT be a real number to raise an exception."
+        )
+
+        # Values.
+        kwargs: dict = {
+            "value": "1",
+            "bound": 0,
+            "include": False,
+            "excpt": True,
+        }
+
+        # Must raise an assertion error.
+        with self.assertRaises(AssertionError, msg=emessage) as _:
+            vnumbers.validate_less_than(**kwargs)
+
+        # Must be a boolean.
+        kwargs["value"] = -1
+
+        vnumbers.validate_less_than(**kwargs)
 
 
 # #############################################################################
