46b0388159b75a171168eac9696e15acf24bdbc2
Andres Garcia Escovar <andrumen1@yahoo.com>
2024-12-20 00:28:38 -0600

    Updated the notation in a more convenient way.

--- src/utilities/general/gtypes.py
+++ src/utilities/general/gtypes.py
@@ -19,25 +19,11 @@ from typing import Union, Type
 
 
 # built-in types.
-builtin = Union[
-    bool, complex, dict, float, frozenset, int, list, set, str, tuple
-]
+builtin_collections = Union[dict, frozenset, list, set, tuple]
+builtin_numbers = Union[complex, float, int]
 
-collections = Union[
-    dict, frozenset, list, set, tuple
-]
+builtin = Union[builtin_collections, builtin_numbers, str]
 
 # Tuple types.
 tbool = tuple[bool, bool]
 treal = tuple[Real, Real]
-
-
-class Dog:
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-
-if __name__ == "__main__":
-
-    dog = Dog("Fido", 5)
-    print(isinstance(dog, builtin))
