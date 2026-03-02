d21bbe92d7c68cdbecaf401d5952e66be8a3d0b6
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-03-02 10:18:19 -0600

    Code was overindented.

--- tests/exceptions/test_ecollections.py
+++ tests/exceptions/test_ecollections.py
@@ -3,9 +3,9 @@
 """
 
 
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Imports
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 # Standard Library.
@@ -17,74 +17,74 @@ from gutilities.exceptions.ecollections import (
 )
 
 
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Classes
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 class TestCollectionErrors(unittest.TestCase):
+    """
+        Tests for the collection errors/exceptions.
+    """
+    # /////////////////////////////////////////////////////////////////////
+    # Test Methods
+    # /////////////////////////////////////////////////////////////////////
+
+    def test_notincollectionerror(self):
         """
-            Tests for the collection errors/exceptions.
+            Tests the NotInCollectionError exception.
         """
-        # /////////////////////////////////////////////////////////////////////
-        # Test Methods
-        # /////////////////////////////////////////////////////////////////////
+        # Error class.
+        mmessage: str = "The error message is not the expected one."
 
-        def test_notincollectionerror(self):
-            """
-                Tests the NotInCollectionError exception.
-            """
-            # Error class.
-            mmessage: str = "The error message is not the expected one."
+        # Auxiliary variables.
+        vobject: int = 6
+        collection: tuple = (1, 2, 3, 4, 5)
 
-            # Auxiliary variables.
-            vobject: int = 6
-            collection: tuple = (1, 2, 3, 4, 5)
+        # Error class.
+        err: NotInCollectionError = NotInCollectionError(
+            None, vobject, collection
+        )
 
-            # Error class.
-            err: NotInCollectionError = NotInCollectionError(
-                None, vobject, collection
-            )
+        print(err.message)
 
-            print(err.message)
+        mexpected: str = (
+            f"The collection is not of the expected length. Object being "
+            f"validated: {vobject}. Collection of possible objects: "
+            f"{collection}."
+        )
 
-            mexpected: str = (
-                f"The collection is not of the expected length. Object being "
-                f"validated: {vobject}. Collection of possible objects: "
-                f"{collection}."
-            )
+        # Check the message is the expected one.
+        self.assertEqual(err.message, mexpected, mmessage)
 
-            # Check the message is the expected one.
-            self.assertEqual(err.message, mexpected, mmessage)
+    def test_wronglengtherror(self):
+        """
+            Tests the WrongLengthError exception.
+        """
+        # Error class.
+        mmessage: str = (
+            "The expected message is not the same as the current message."
+        )
 
-        def test_wronglengtherror(self):
-            """
-                Tests the WrongLengthError exception.
-            """
-            # Error class.
-            mmessage: str = (
-                "The expected message is not the same as the current message."
-            )
+        # Auxiliary variables.
+        lexpected: int = 6
+        lcurrent: int = len((1, 2, 3, 4, 5))
 
-            # Auxiliary variables.
-            lexpected: int = 6
-            lcurrent: int = len((1, 2, 3, 4, 5))
+        # Error class.
+        err: WrongLengthError = WrongLengthError(None, lcurrent, lexpected)
+        mexpected: str = (
+            f"The collection is not of the expected length. Current "
+            f"length of the collection: {lcurrent}. Expected length: "
+            f"{lexpected}."
+        )
 
-            # Error class.
-            err: WrongLengthError = WrongLengthError(None, lcurrent, lexpected)
-            mexpected: str = (
-                f"The collection is not of the expected length. Current "
-                f"length of the collection: {lcurrent}. Expected length: "
-                f"{lexpected}."
-            )
-
-            # Check the message is the expected one.
-            self.assertEqual(err.message, mexpected, mmessage)
+        # Check the message is the expected one.
+        self.assertEqual(err.message, mexpected, mmessage)
 
 
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Main Program
-# #############################################################################
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 
 if __name__ == "__main__":
