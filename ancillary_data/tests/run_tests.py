f8e186c8a4fb1e61fc11a389c383fd4a003543a9
Andres Garcia Escovar <andrumen1@yahoo.com>
2026-04-01 12:44:12 -0500

    Added script to validate files.

---
+++ ancillary_data/tests/run_tests.py
@@ -0,0 +1,131 @@
+"""
+    Runs the tests from the package.
+"""
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Imports
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+# Standard library.
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+# User.
+from gutilities.context_managers.cworking import WorkingDirectory
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Global Variables
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+# Paths.
+PATH_ROOT: Path = Path(__file__).parent.parent.parent
+PATH_TESTS: Path = PATH_ROOT / "tests"
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Functions - Auxiliary
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+def _get_arguments() -> dict:
+    """
+        Gets the arguments from the command line arguments.
+
+        :return: The dictionary with the options chosen from the command
+         line arguments.
+    """
+    return {}
+
+
+def _get_files_tests_interactive(options: list, files: list) -> tuple:
+    """
+        Validates that the list of entries is valid; i.e., that every entry
+        contains an integer number in the proper range.
+
+        :param options: The list with the numbers associated with the files to
+         be validated. The numbers do not need to be unique, since they will be
+         filtered out.
+
+        :param files: The list with the available test files.
+    """
+    # Auxiliary variables.
+    message: str = ""
+    results: set = set()
+
+    # Check all the numbers.
+    for i, option in enumerate(options):
+        # Turn into integers.
+        try:
+            index: int = int(option.strip())
+
+            if not (1 <= index <= len(files)):
+                raise ValueError()
+
+            results.add(f"{files[index - 1]}")
+
+        except (IndexError, ValueError):
+            message += (
+                f"The {i}th entry ({repr(option)}) could not be converted "
+                f"into an integer, or is out of range; valid range: [1, "
+                f"{len(files)}], both ends included. "
+            )
+
+    # Raise the error if needed.
+    if message != "":
+        raise ValueError(message.strip())
+
+    return tuple(sorted(results, key=lambda x: x.lower()))
+
+
+def _tests_interactive() -> tuple:
+    """
+        Runs the tests in an interactive way.
+    """
+    # Auxiliary variables.
+    base: str = "\n    "
+    cardw: str = "test_*.py"
+    files: list = sorted(
+        (x for x in Path.cwd().rglob(cardw) if x.is_file()),
+        key=lambda x: f"{x}".lower()
+    )
+    string: str = base.join(
+        f"{i:2d}. {f'{x}'.replace(f'{PATH_TESTS}', '')[1:]}"
+        for i, x in enumerate(files, start=1)
+    )
+
+    # Get the files to be examined.
+    options: list = input(
+        f"Select the files desired to be checked:{base}{string}\nFor multiple "
+        f"files, use the numbers separated by commas: "
+    ).split(",")
+
+    # Validate the inputs.
+    return _get_files_tests_interactive(options, files)
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Main Function
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+def run() -> None:
+    """
+        Run the tests.
+    """
+    # Get the dictionary of arguments.
+    _tests_interactive()
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# Main Program
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+if __name__ == "__main__":
+    # Set the working directory.
+    with WorkingDirectory(PATH_TESTS):
+        run()
